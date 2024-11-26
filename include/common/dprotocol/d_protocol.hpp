11d8f4696635c7eb81820b9680e048ed6a7010b8
Xiangfu Li <xiangfuli96@gmail.com>
2024-11-25 20:01:00 -0500

    Change message length to uint32_t to fit camera data

--- include/common/dprotocol/d_protocol.hpp
+++ include/common/dprotocol/d_protocol.hpp
@@ -1,6 +1,7 @@
 #ifndef D_PROTOCOL_HPP
 #define D_PROTOCOL_HPP
 
+#include <iostream>
 #include "dprotocol/circular_byte_array.hpp"
 #include "dprotocol/d_protocol_message.hpp"
 
@@ -12,7 +13,7 @@ public:
   // preserved space
   uint16_t message_header;
   uint8_t protocol_version;
-  uint16_t messageLength;
+  uint32_t messageLength;
 
   uint8_t retrieveMessage(CircularByteArray *cba, DProtocolMessage *message);
 
--- src/d_protocol.cpp
+++ src/d_protocol.cpp
@@ -9,7 +9,7 @@ DProtocal::~DProtocal() {}
 uint8_t DProtocal::retrieveMessage(CircularByteArray *cba, DProtocolMessage *message) {
   // first get 2 bytes from the cba to check if it's the d protocol message
 
-  if (cba->size() <= 6) {
+  if (cba->size() <= 8) {
     // no enough data
     return 1;
   }
@@ -17,23 +17,23 @@ uint8_t DProtocal::retrieveMessage(CircularByteArray *cba, DProtocolMessage *mes
   if (cba->peek((uint8_t *)&message_header, 2, 0) == 0 && message_header == MESSAGE_START_MAGIC_NUMBER) {
     // check the protocol version
     if (cba->peek(&protocol_version, 1, 2) == 0 && protocol_version == 1) {
-      cba->peek((uint8_t *)&messageLength, 2, 4);
+      cba->peek((uint8_t *)&messageLength, 4, 4);
       // then retrieve the message and translate to d protocol message
       message->protocolVersion = 1;
       message->clientId = 1;
       message->length = messageLength;
 
       // check if current message has already been transformed completely
-      if (cba->size() == (8 + messageLength)
-          || (cba->peek((uint8_t *)&message_header, 2, 8 + messageLength) == 0 && message_header == MESSAGE_START_MAGIC_NUMBER)) {
-        cba->peek((uint8_t *)&message->type, 2, 6);
+      if (cba->size() == (10 + messageLength)
+          || (cba->peek((uint8_t *)&message_header, 2, 10 + messageLength) == 0 && message_header == MESSAGE_START_MAGIC_NUMBER)) {
+        cba->peek((uint8_t *)&message->type, 2, 8);
         message->body = new uint8_t[messageLength];
-        cba->peek(message->body, messageLength, 8);
-        cba->remove(messageLength + 8);
+        cba->peek(message->body, messageLength, 10);
+        cba->remove(messageLength + 10);
         return 0;
-      } else if (cba->size() < (8 + messageLength)) {
+      } else if (cba->size() < (10 + messageLength)) {
         return 1;
-      } else if (cba->peek((uint8_t *)&message_header, 2, 8 + messageLength) == 0 && message_header != MESSAGE_START_MAGIC_NUMBER) {
+      } else if (cba->peek((uint8_t *)&message_header, 2, 10 + messageLength) == 0 && message_header != MESSAGE_START_MAGIC_NUMBER) {
         // encounter illegal message head
         return 10;
       }
@@ -48,24 +48,28 @@ uint8_t DProtocal::wrapMessage(uint8_t *&messageBinary, uint16_t messageType, ui
   *((uint16_t *) messageBinary) = MESSAGE_START_MAGIC_NUMBER;
   *((uint8_t *) messageBinary + 2) = 1;
   *((uint8_t *) messageBinary + 3) = 1;
-  *((uint16_t *) (messageBinary + 4)) = size;
-  *((uint16_t *) (messageBinary + 6)) = messageType;
-  memcpy(messageBinary + 8, data, size);
+  *((uint32_t *) (messageBinary + 4)) = size;
+  *((uint16_t *) (messageBinary + 8)) = messageType;
+  memcpy(messageBinary + 10, data, size);
   return 0;
 }
 
 
 void DProtocal::cleanCBA(CircularByteArray *cba) {
   uint16_t head;
+  uint16_t clean_num = 0;
   while (cba->size() > 0) {
     if (cba->peek((uint8_t *) &head, 2, 0) == 0) {
       if (head == MESSAGE_START_MAGIC_NUMBER) {
         return;
       } else {
+        clean_num++;
         cba->remove(1);
       }
     } else { // size of the bytes inside the buffer is less than 2.
+      clean_num++;
       cba->remove(1);
     }
   }
+  std::cout << "Cleaning " << clean_num << std::endl;
 }
